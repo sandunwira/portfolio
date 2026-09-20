995fa3e109b860c8d8574489f7b9a26db656979f
sandunwira <79461263+sandunwira@users.noreply.github.com>
2026-09-20 11:32:25 +0530

    feat: update global styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function HomePage() {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen py-2">
 			<h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
-			<p className="text-lg text-gray-600">
+			<p className="text-lg text-zinc-400">
 				Currently under construction, but feel free to explore and check back later for updates!
 			</p>
 		</div>
